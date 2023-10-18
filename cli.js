b216f202f0da8be2e8d0f9b4fbf03d3fe50fcf80
simonjur <118170619+simonjur@users.noreply.github.com>
2023-10-18 12:55:28 +0200

    third

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
 var minimatch = require('minimatch');
 var expand = require('brace-expansion'); // ???
 var glob = require('glob'); // ???
-var glob = require('path-scurry'); //???
+var p = require('path-scurry'); //???
